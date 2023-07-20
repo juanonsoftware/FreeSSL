e2aad5cbf933b89c6e767b821d8388513b2fac99
juanonsoftware <juanonsoftware@gmail.com>
2023-07-20 14:06:44 +0700

    FreeSSL web v0.9

---
+++ EasyFreeSSLSite/HttpRequestExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyFreeSSLSite
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetCurrentUrl(this HttpRequest request)
+        {
+            return $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}";
+        }
+    }
+}
