6462465c33b4d0ecfc6e011e337f25f65a97512b
juanonsoftware <juanonsoftware@gmail.com>
2023-07-20 14:56:27 +0700

    Move content out of razor page to make deployment easy How to read a file from root folder of application

---
+++ EasyFreeSSLSite/AppDataExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyFreeSSLSite
+{
+    public static class AppDataExtensions
+    {
+        public static string ReadAppDataFile(this IWebHostEnvironment host, string fileName)
+        {
+            var appData = Path.Combine(host.ContentRootPath, "AppData");
+            return File.ReadAllText(Path.Combine(appData, "IndexContent.html"));
+        }
+    }
+}
--- EasyFreeSSLSite/Pages/Index.cshtml.cs
+++ EasyFreeSSLSite/Pages/Index.cshtml.cs
@@ -1,19 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EasyFreeSSLSite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _host;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment host)
         {
             _logger = logger;
+            _host = host;
         }
 
         public void OnGet()
         {
+            ViewData["IndexContent"] = _host.ReadAppDataFile("IndexContent.html");
         }
     }
 }
